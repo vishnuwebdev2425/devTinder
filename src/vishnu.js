d8e0f04df4bb5f33ee030358fb19d6b5fef5d83d
vishnuwebdev2425 <akkupallivishnu2424@gmail.com>
2025-09-23 11:24:40 +0530

    error handlers

--- src/vishnu.js
+++ src/vishnu.js
@@ -1,33 +1,23 @@
 const express=require('express');
 const app=express();
 
-
-app.get("/user",(req,res)=>{
-    res.send({firstname:"Vishnu",lastname:"Yadav"});
-
+app.use("/user",(req,res,next)=>{
+    console.log("Intial Route Handler");
+    next()
+    
+},[(req,res,next)=>{
+    console.log("Second Route Handler");
+  
+    next()
+},(req,res,next)=>{
+    console.log("This is the third route handler");
+    next()
+    res.send("Finally we get Successed");
+}],(req,res)=>{
+    res.send("This is I want to executed")
+    
+    console.log("Executed Successfully")
 })
-
-app.patch("/user",(req,res)=>{
-    res.send({firstname:"Sanjana"})
-})
-
-app.put("/user",(req,res)=>{
-    res.send("User Details Updated to the Database Successfully");
-
-})
-app.get("/usersdetails",(req,res)=>{
-    console.log(req.query);
-    res.send("User Details Fetched Successfully");
-})
-app.get("/useraddress/:city/:state",(req,res)=>{
-    console.log(req.params);
-    res.send("User Address Fetched Successfully");
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("User deleted Successfully");
-})
-
 
 app.listen(7777,()=>{
     console.log("App Is listening on port 7777");

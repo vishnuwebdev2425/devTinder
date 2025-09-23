f758a7ca137c2b0ac75f49a8828ed0b1f6098621
vishnuwebdev2425 <akkupallivishnu2424@gmail.com>
2025-09-23 11:15:37 +0530

    queery-params

--- src/vishnu.js
+++ src/vishnu.js
@@ -1,13 +1,33 @@
 const express=require('express');
 const app=express();
 
-app.use("/home",(req,res)=>{
-    res.send("Hello from vishnus server")
-})
-app.use("/profile",(req,res)=>{
-    res.send("You are entered into the profie page")
+
+app.get("/user",(req,res)=>{
+    res.send({firstname:"Vishnu",lastname:"Yadav"});
 
 })
+
+app.patch("/user",(req,res)=>{
+    res.send({firstname:"Sanjana"})
+})
+
+app.put("/user",(req,res)=>{
+    res.send("User Details Updated to the Database Successfully");
+
+})
+app.get("/usersdetails",(req,res)=>{
+    console.log(req.query);
+    res.send("User Details Fetched Successfully");
+})
+app.get("/useraddress/:city/:state",(req,res)=>{
+    console.log(req.params);
+    res.send("User Address Fetched Successfully");
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("User deleted Successfully");
+})
+
 
 app.listen(7777,()=>{
     console.log("App Is listening on port 7777");
